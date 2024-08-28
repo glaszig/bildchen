098bd755d9d3eb92be0cec474d0efc47ed1602c3
glaszig <mail@glasz.org>
2024-08-28 00:49:27 -0300

    protected => private

--- lib/bildchen/resolver.rb
+++ lib/bildchen/resolver.rb
@@ -15,37 +15,37 @@ module Bildchen
       end
     end
 
-  protected
+    private
 
-    # Recursively tries to HEAD-request the path
-    def _resolve path, **options
-      candidates(path).detect do |path|
-        downloadable? path, **options
-      end || _resolve(File.dirname(path), options)
-    end
-
-    # HEAD-requests a file
-    def downloadable? path, **options
-      (200..299).include? http(**options).head(path, 'User-Agent' => USER_AGENT).code.to_i
-    end
-
-    # Interpolates path with '%{favicon}' and returns candidates to probe
-    def candidates path
-      apply_icon_names File.join File.dirname(path), '%{favicon}'
-    end
-
-    def apply_icon_names template
-      ICON_NAMES.map { |name| template % { favicon: name } }
-    end
-
-    # HTTP client factory
-    def http timeout: Bildchen.timeout
-      @conn ||= Net::HTTP.new(uri.host, uri.port).tap do |conn|
-        conn.open_timeout = timeout
-        conn.read_timeout = timeout
-        conn.use_ssl = (uri.scheme == 'https')
+      # Recursively tries to HEAD-request the path
+      def _resolve path, **options
+        candidates(path).detect do |path|
+          downloadable? path, **options
+        end || _resolve(File.dirname(path), options)
       end
-    end
+
+      # HEAD-requests a file
+      def downloadable? path, **options
+        (200..299).include? http(**options).head(path, 'User-Agent' => USER_AGENT).code.to_i
+      end
+
+      # Interpolates path with '%{favicon}' and returns candidates to probe
+      def candidates path
+        apply_icon_names File.join File.dirname(path), '%{favicon}'
+      end
+
+      def apply_icon_names template
+        ICON_NAMES.map { |name| template % { favicon: name } }
+      end
+
+      # HTTP client factory
+      def http timeout: Bildchen.timeout
+        @conn ||= Net::HTTP.new(uri.host, uri.port).tap do |conn|
+          conn.open_timeout = timeout
+          conn.read_timeout = timeout
+          conn.use_ssl = (uri.scheme == 'https')
+        end
+      end
 
   end
 
