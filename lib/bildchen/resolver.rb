4a0b81414f049d7d0b27f202a64930971203cdfb
glaszig <mail@glasz.org>
2024-08-28 00:44:34 -0300

    keyword arguments

--- lib/bildchen/resolver.rb
+++ lib/bildchen/resolver.rb
@@ -11,8 +11,8 @@ module Bildchen
     end
 
     # Returns the url at which a favicon was supposedly found
-    def resolve options = { timeout: Bildchen.timeout }
-      if path = _resolve(uri.path, options)
+    def resolve **options
+      if path = _resolve(uri.path, **options)
         dupe = uri.dup
         dupe.path = path
         dupe.to_s
@@ -22,15 +22,15 @@ module Bildchen
   protected
 
     # Recursively tries to HEAD-request the path
-    def _resolve path, options = {}
+    def _resolve path, **options
       candidates(path).detect do |path|
-        downloadable? path, options
-      end || resolve(File.dirname(path), options)
+        downloadable? path, **options
+      end || _resolve(File.dirname(path), options)
     end
 
     # HEAD-requests a file
-    def downloadable? path, options = {}
-      (200..299).include? http(options).head(path, 'User-Agent' => USER_AGENT).code.to_i
+    def downloadable? path, **options
+      (200..299).include? http(**options).head(path, 'User-Agent' => USER_AGENT).code.to_i
     end
 
     # Interpolates path with '%{favicon}' and returns candidates to probe
@@ -43,10 +43,10 @@ module Bildchen
     end
 
     # HTTP client factory
-    def http options = {}
+    def http timeout: Bildchen.timeout
       @conn ||= Net::HTTP.new(uri.host, uri.port).tap do |conn|
-        conn.open_timeout = options[:timeout]
-        conn.read_timeout = options[:timeout]
+        conn.open_timeout = timeout
+        conn.read_timeout = timeout
         conn.use_ssl = (uri.scheme == 'https')
         conn.verify_mode = self.ssl_verify_mode
       end
